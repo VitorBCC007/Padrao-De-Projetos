f74f3b39239ed1879667d571950638747a01cb0e
João Vítor <118393027+VitorBCC007@users.noreply.github.com>
2024-09-10 23:33:41 -0300

    Update and rename models.py to modes.py

--- DAO/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-
-class Product:
-    def __init__(self, product_id, name, price):
-        self.product_id = product_id
-        self.name = name
-        self.price = price
---
+++ DAO/modes.py
@@ -0,0 +1,5 @@
+class Produto:
+    def __init__(self, id_produto, nome, preco):
+        self.id_produto = id_produto
+        self.nome = nome
+        self.preco = preco
